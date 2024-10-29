dfcf331d2c73f519d36ba14cd86696557b7d295a
KWONYUBIN <71390019+KWONYUBIN@users.noreply.github.com>
2024-10-29 17:30:16 +0900

    최초 커밋

---
+++ src/main/java/com/example/prjjsp20241029/PrjJsp20241029Application.java
@@ -0,0 +1,13 @@
+package com.example.prjjsp20241029;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjJsp20241029Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjJsp20241029Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prjjsp20241029/PrjJsp20241029ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prjjsp20241029;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjJsp20241029ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
