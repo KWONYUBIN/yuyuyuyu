7c5432286f5cd9e3dd3010a556835146c4413be6
KWONYUBIN <rnjsdbqls112@naver.com>
2024-10-30 12:17:07 +0900

    페이징 연습용 복붙

--- sql/board.sql
+++ sql/board.sql
@@ -6,9 +6,23 @@ CREATE TABLE board
     title    VARBINARY(200)  NOT NULL,
     content  VARBINARY(5000) NOT NULL,
     writer   VARBINARY(200)  NOT NULL,
-    inserted DATETIME NOT NULL DEFAULT NOW()
+    inserted DATETIME        NOT NULL DEFAULT NOW()
 );
 
 SELECT *
 FROM board;
+
+SELECT COUNT(*)
+FROM board;
+
+# 페이징 연습용 게시물 복붙
+INSERT INTO board
+    (title, content, writer)
+SELECT title, content, writer
+FROM board;
+
+
+
+
+
 
